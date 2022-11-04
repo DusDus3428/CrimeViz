d18e0dd60cbe8b6cbfd055e879de2da9b8e64565
DusDus3428 <dustinnoah.young@gmail.com>
2022-11-04 13:25:49 +0100

    Rearranged class fields

--- python_data_gatherer/python_data_gatherer_app/model/uniform_crime.py
+++ python_data_gatherer/python_data_gatherer_app/model/uniform_crime.py
@@ -1,14 +1,14 @@
-class Crime:
+class UniformCrime:
     def __init__(self, description, area, location, premise, victim_age, victim_sex, victim_descent, weapon_used,
                  status, report_date, occurrence_date, occurrence_time, latitude, longitude):
         self.description = description
+        self.weapon_used = weapon_used
         self.area = area
         self.location = location
         self.premise = premise
         self.victim_age = victim_age
         self.victim_sex = victim_sex
         self.victim_descent = victim_descent
-        self.weapon_used = weapon_used
         self.status = status
         self.report_date = report_date
         self.occurrence_date = occurrence_date
